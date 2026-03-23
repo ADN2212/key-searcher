92940a8c25ddef20181ef3d267dcbdf929de18fb
Juan Adonis <adn221294@gmail.com>
2026-03-23 16:12:12 -0400

    add some optimizations in the searhckey function

--- index.js
+++ index.js
@@ -1,52 +1,43 @@
 const getObjectsKeys = (obj) => Object.keys(obj);
 
-function changeNullforFalse(obj){
-	const objStr = JSON.stringify(obj).replaceAll('null', 'false')
-	return JSON.parse(objStr);
+function searchForKey(obj, key, newObject) {
+  if (key in obj) {
+    newObject[key] = obj[key];
+    return null;
+  }
+
+  const objectsValues = getObjectsKeys(obj).filter(
+    (key) => obj[key] !== null && typeof obj[key] === "object",
+  );
+
+  const numberOfObjectsValues = objectsValues.length;
+
+  if (numberOfObjectsValues === 0) return null;
+
+  let currentKey = null;
+
+  for (let i = 0; i < numberOfObjectsValues; i++) {
+    currentKey = objectsValues[i];
+    searchForKey(obj[currentKey], key, newObject);
+  }
+
+  return null;
 }
 
-function searchForKey(obj, key, newObject){
+function searchIn(obj, ...keysToSearch) {
+  let flattenedJSON = {};
+  const len = keysToSearch.length;
+  let currentKey = null;
 
-	if (key in obj) {
-		newObject[key] = obj[key]
-		return null
-	}
+  for (let i = 0; i < len; i++) {
+    currentKey = keysToSearch[i];
+    searchForKey(obj, currentKey, flattenedJSON);
+    if (!getObjectsKeys(flattenedJSON).includes(currentKey)) {
+      flattenedJSON[currentKey] = undefined;
+    }
+  }
 
-	const objectsValues = getObjectsKeys(obj).filter(
-		(key) => {
-			return typeof obj[key] === 'object';
-		}
-	)
-
-	const numberOfObjectsValues = objectsValues.length
-
-	if (numberOfObjectsValues === 0) return null;
-
-	let currentKey = null
-
-	for(let i = 0; i < numberOfObjectsValues ; i++){
-		currentKey = objectsValues[i];
-		searchForKey(obj[currentKey], key, newObject);
-	}
-
-	return null;
-}
-
-function searchIn(obj, ...keysToSearch){
-	const objectWhitoutNulls = changeNullforFalse(obj) 
-	let flattenedJSON = {}
-	const len = keysToSearch.length
-	let currentKey = null
-
-	for(let i = 0; i < len ; i++){
-	 	currentKey = keysToSearch[i];
-	 	searchForKey(objectWhitoutNulls, currentKey, flattenedJSON);
-	 	if (!getObjectsKeys(flattenedJSON).includes(currentKey)){
-	 		flattenedJSON[currentKey] = undefined;
-	 	}
-	 }
-
-	return flattenedJSON;
+  return flattenedJSON;
 }
 
 export default searchIn;
